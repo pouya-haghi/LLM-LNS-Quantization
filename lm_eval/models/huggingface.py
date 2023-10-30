2fded212b24d7aac45a5f3e5eda2c58e15b1cb62
Pouya Haghi <pouya@dhcp-10-5-163-109.wireless.rochester.edu>
2023-10-30 19:51:21 -0400

    just using different data types

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -834,8 +834,7 @@ class HuggingFaceAutoLM(BaseLM):
                         model_kwargs[
                             "bnb_4bit_use_double_quant"
                         ] = bnb_4bit_use_double_quant
-            # model = self.AUTO_MODEL_CLASS.from_pretrained(
-            model = self.T5ForConditionalGeneration.from_pretrained(
+            model = self.AUTO_MODEL_CLASS.from_pretrained(
                 pretrained,
                 revision=revision + ("/" + subfolder if subfolder is not None else ""),
                 low_cpu_mem_usage=low_cpu_mem_usage,
