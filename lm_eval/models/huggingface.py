d555d6ed9da427675c695fa3aedad923e5ef826e
Pouya Haghi <pouya@dhcp-10-5-144-133.wireless.rochester.edu>
2023-11-06 16:06:33 -0500

    just using different data types

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -308,7 +308,7 @@ class HuggingFaceAutoLM(BaseLM):
             counter.increase() #$$$
             return output
 
-        # EXCLUDED_ACTIVATIONS = (nn.ReLU, nn.Tanh, nn.GELU, nn.Sigmoid, nn.Softmax, nn.LeakyReLU, nn.PReLU)
+        EXCLUDED_ACTIVATIONS = (nn.ReLU, nn.Tanh, nn.GELU, nn.Sigmoid, nn.Softmax, nn.LeakyReLU, nn.PReLU)
         for name, module in self.model.named_modules():
             if not isinstance(module, nn.ModuleList) and not list(module.children()) and "intermediate_act_fn" not in name and not isinstance(module, nn.LayerNorm) and not isinstance(module, nn.Dropout) and not any(isinstance(module, activation) for activation in EXCLUDED_ACTIVATIONS):
                 module.register_forward_hook(activation_hook)
